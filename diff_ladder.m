fb3f2db8c1aafc8bbbee4e2dafe201fc49da09be
Zach <zgrey334@gmail.com>
2018-10-30 00:22:51 -0600

    Smooth 2 tensor animations

--- diff_ladder.m
+++ diff_ladder.m
@@ -1,6 +1,10 @@
 function V1 = diff_ladder(p0,p1,V0,Exp,Log,N)
+if norm(p0-p1) < 1e-10
+    V1 = V0;
+else
 % discretize geodesic
-t = linspace(0,1,N)'; v = Log(p0,p1);
+t = linspace(0,1,N)'; v = Log(p0,p1); 
+if N ~= 1, dt = 1/(N-1); end
 geo = Exp(t*norm(v),v,p0); 
 % initialize
 V1 = V0;
@@ -15,5 +19,7 @@ for i=1:N-1
     P2    = Exp(tau,V1,P0); P3    = Exp(-tau,V1,P0);
     Vlog2 = Log(P1,P2);     Vlog3 = Log(P1,P3);
     V1 = 1/(2*tau)*(Vlog2 - Vlog3);
+
+end
 
 end
---
+++ schilds_ladder.m
@@ -0,0 +1,28 @@
+function V1 = schilds_ladder(p0,p1,V0,Exp,Log,N)
+if norm(p0-p1) < 1e-10
+    V1 = V0;
+else
+% discretize geodesic
+t = linspace(0,1,N)'; v = Log(p0,p1); 
+geo = Exp(t*norm(v),v,p0); 
+% initialize
+V1 = V0;
+
+for i=1:N-1
+
+    % sample geodesic discretization
+    P0 = geo(i,:); P1 = geo(i+1,:);
+
+    % Schild's ladder
+    % scale rungs on ladder
+    tau = 0.1;
+    % geodesic from first point in direction
+    P2  = Exp(1*tau,V1,P0);
+    % "mid-point" between P2 and P1
+    p   = Exp(0.5*tau,Log(P1,P2),P1);
+    % vector mapping to twice the length through mid-point
+    V1  = 1/tau*Log(P1,Exp(2*tau,Log(P0,p),P0));
+
+end
+
+end
